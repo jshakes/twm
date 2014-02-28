97260d7e9969a8183fa1a958d80fbf85b3d52ae5
James Shakespeare <j@jshakespeare.com>
2014-02-28 00:21:04 -0500

    tracks from search results can be added into the playlist slots

--- public/js/templates.js
+++ public/js/templates.js
@@ -1,3 +1,3 @@
 window.TWM.templates = {
-  "track-search-result": "<h5 class=\"track-search-result-title\"><%= title %></h5><span class=\"track-search-result-duration\"><%= minutes %></span><span class=\"track-search-result-source\"><%= source %></span>"
+  "track-search-result": "<h5 class=\"track-search-result-title\"><%= title %></h5><span class=\"track-search-result-duration\"><%= minutes %></span><span class=\"track-search-result-source\"><%= source %></span><button class=\"track-search-result-add\">Add to playlist</button>"
 }
--- src/js/apps/playlist/create/trackSearch/trackSearch_view.js
+++ src/js/apps/playlist/create/trackSearch/trackSearch_view.js
@@ -6,15 +6,24 @@ TWM.module("Playlists.Create.TrackSearch", function(TrackSearch, TWM, Backbone, 
     className: "track-search-result",
     events: {
       "click .preview-track": "previewTrack",
-      "click .add-track": "addTrack"
+      "click .track-search-result-add": "addTrack"
     },
     previewTrack: function(){
 
       // Play the middle 10 seconds of the song
     },
-    addTrack: function(){
+    addTrack: function(e){
 
-      // Add this track to the playlist and close the search modal
+      e.preventDefault();
+      var trackIndex = $(".playlist-track-select:checked").data("index");
+      var $trackIdInput = $("#track-" + trackIndex + "-id");
+      var $trackIdSource = $("#track-" + trackIndex + "-source");
+      var $trackIdPlaceholder = $("#track-" + trackIndex + "-placeholder");
+      // Add this track to the currently selected track slot
+      $trackIdInput.val(this.model.get("id"));
+      $trackIdSource.val(this.model.get("source"));
+      // Show the track info in the placeholder
+      $trackIdPlaceholder.val(this.model.get("title"));
     }
   });
 
