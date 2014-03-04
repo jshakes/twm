cdc40344b8fa7818cda12df9754ef2834b6e5e7c
James Shakespeare <j@jshakespeare.com>
2014-03-03 22:59:24 -0500

    moving to swig because it's a million times better

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ Dependencies
 ============================================
 */
 var routes = require("./routes");
-var engine = require("ejs-locals");
+var swig = require('swig');
 
 /*
 ============================================
@@ -19,8 +19,11 @@ console.log("Express server listening on port 3000");
 app.configure(function(){
 
   // Set up the template engine
-  app.engine("ejs", engine);
-  app.set("view engine", "ejs");
+  app.engine('html', swig.renderFile);
+  app.set('view engine', 'html');
+  app.set('views', __dirname + '/views');
+  app.set('view cache', false);
+  swig.setDefaults({ cache: false });
   // Serve static files from /public
   app.use(express.static(__dirname + "/public"));
   // Use URLencoded for post data
