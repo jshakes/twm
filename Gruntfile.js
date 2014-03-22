b745aad85af184a85e8687341039c8ca21c8fe2a
James Shakespeare <j@jshakespeare.com>
2014-03-21 21:14:53 -0400

    lulz

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,7 +37,7 @@ module.exports = function(grunt){
           "./src/lib/backbone/backbone.js",
           "./src/lib/backbone.marionette/lib/backbone.marionette.js",
           "./src/lib/ejs/ejs.js",
-          "./src/lib/popcorn-complete.min.js"
+          "./src/popcorn-complete.min.js"
           ],
         dest: "./public/js/vendors.js"
       },
